bbb5b02a708b4eb20952df2c8ab877488fc731c0
Taro Hoshino <no_taro1990@outlook.com>
2021-02-28 22:28:18 +0900

    Added Array files

---
+++ __tests__/array.test.js
@@ -0,0 +1,5 @@
+const { test } = require("@jest/globals");
+
+test('hoge', () => {
+  expect(1+2).toBe(3);
+});
---
+++ scripts/array.js
@@ -0,0 +1,8 @@
+let a = [10, 11, 12];
+let b = ['first', 'second', 3];
+
+let length = a.length;
+
+let index = a.indexOf(11);
+
+
